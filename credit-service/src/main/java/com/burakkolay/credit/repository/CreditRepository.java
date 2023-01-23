6af309c4331c06eef11955f81bb3214e4db259b2
burak <burakkolay487@gmail.com>
2023-01-23 10:37:51 +0300

    Added native query for credit waiting

--- credit-service/src/main/java/com/burakkolay/credit/repository/CreditRepository.java
+++ credit-service/src/main/java/com/burakkolay/credit/repository/CreditRepository.java
@@ -3,8 +3,15 @@ package com.burakkolay.credit.repository;
 
 import com.burakkolay.credit.model.entity.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CreditRepository extends JpaRepository<Credit,Long> {
+
+    @Query(value = "SELECT * FROM credit WHERE credit_result=1",nativeQuery = true)
+    List<Credit> getAllCreditWaiting();
+
 }
