7a850cb31d70dae9e291a4a985e860532096a546
burak <burakkolay487@gmail.com>
2023-01-23 10:39:37 +0300

    Created send SMS with twilio

--- credit-service/src/main/java/com/burakkolay/credit/services/TwilioService.java
+++ credit-service/src/main/java/com/burakkolay/credit/services/TwilioService.java
@@ -1,23 +1,31 @@
 package com.burakkolay.credit.services;
 
 import com.burakkolay.credit.model.entity.Applicant;
+import com.burakkolay.credit.model.entity.Credit;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwilioService {
-    public static final String ACCOUNT_SID="AC5d320aeddc050e5b70eeb89ada338967";
-    public static final String AUTH_TOKEN="9618763b529db90847cce8811ae0dfc6";
 
-    public void sendSMS(Applicant applicant){
+    @Value("${twilio.account.sid}")
+    private String ACCOUNT_SID;
+
+    @Value("${twilio.auth.token}")
+    private String AUTH_TOKEN;
+
+    public void sendSMS(Applicant applicant, Credit credit){
         Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Dear"+applicant.getFirstName()+" "+ applicant.getLastName()+", your credit rating is"+applicant.getCreditRating());
-
-        Message message = Message.creator(new PhoneNumber("+905369378309"),new PhoneNumber("+17695532440"),stringBuilder.toString()).create();
-
+        Message message = Message.creator(new PhoneNumber(applicant.getPhoneNumber()),new PhoneNumber("+17695532440"),
+                "Dear" + applicant.getFirstName() + " " + applicant.getLastName() + ", your credit result is " +
+                credit.getCreditResult()+" and your credit limit is "+ credit.getCreditBalance()).create();
     }
 }
