6c9f2cf8b67fa010a407b7135bc0eb145603c673
burak <burakkolay487@gmail.com>
2023-01-25 18:44:05 +0300

    get credits query done

--- credit-service/src/main/java/com/burakkolay/credit/repository/CreditRepository.java
+++ credit-service/src/main/java/com/burakkolay/credit/repository/CreditRepository.java
@@ -4,6 +4,7 @@ package com.burakkolay.credit.repository;
 import com.burakkolay.credit.model.entity.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,9 @@ public interface CreditRepository extends JpaRepository<Credit,Long> {
 
     @Query(value = "SELECT * FROM credit WHERE credit_result=1",nativeQuery = true)
     List<Credit> getAllCreditWaiting();
+
+    @Query(value = "SELECT * FROM credit WHERE applicant_id=(SELECT id FROM applicant WHERE identification_number=(:id))",nativeQuery = true)
+    List<Credit> getCreditsByApplicantId(@Param("id")Long id);
+
 
 }
