bdaa1445b12c9ca4003ece83db72d0d7b2d9ef7f
burak <burakkolay487@gmail.com>
2023-01-28 11:59:07 +0300

    Added sql for create database

---
+++ init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE credit_application_db;
+    INSERT INTO users VALUES [5,null,null,"$2a$12$SHFCEm0KYDMEm/BcJIXbyeeQ4CrzGHDfbEnpmOhk0z7WxtpK9I5Ry",0,"admin"];
+CREATE DATABASE credit_rating_application_db;
