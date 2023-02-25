0431017fde8c62d6633c7e904f05e0b6f1655e86
BurakKolay <burakkolay487@gmail.com>
2023-02-25 19:43:57 +0300

    applicant service test

---
+++ credit-service/src/test/java/com/burakkolay/credit/services/ApplicantServiceTest.java
@@ -0,0 +1,28 @@
+package com.burakkolay.credit.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicantServiceTest {
+
+    @Test
+    void getAllApplicants() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void addCreditToApplicant() {
+    }
+}
